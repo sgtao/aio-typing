1828ff734eca999751de556329153d97764c67a8
Shogo Ogami <sg.tao.so@gmail.com>
2023-03-05 20:33:33 +0900

    update public for trial to play audio files

--- public/scripts/typingGame.js
+++ public/scripts/typingGame.js
@@ -4,33 +4,48 @@
     const targetWords = ["red", "pink", "blue", "yellow", "green"];
     const target = document.querySelector("#target");
     const result = document.querySelector("#result");
-    let words;
+    let sectionContents = [];
+    let contents_index = [];
     let word;
     let wordUpper;
     let wordLower;
     let loc = 0; // target location
     // setTargetWords : assetsから読み込んだオブジェクトをtargetWordsにセット
-    async function setWords() {
+    async function setContents() {
         let allInOne = await resourceAllinOne.loadResource("assets-sample.json");
         console.log('at setWords ' + allInOne);
         console.log(typeof(allInOne));
-        let _words = [];
+        let ctx = [];
+        contents_index = [];
+        let _index = 0;
         allInOne.forEach(item => {
-            console.log(item.englishText);
-            _words.push(item.englishText);
+            console.log(item);
+            ctx.push({ "index": item.index, "word": item.englishText});
+            contents_index.push(_index++);
         });
-        console.log(_words);
-        return _words;
+        console.log(ctx);
+        console.log(contents_index);
+        return {ctx};
     }
     // setWord：次の単語をセットする
     function setWord(target) {
+        console.dir(sectionContents.ctx);
+        console.dir(typeof(sectionContents.ctx));
         // word = words[Math.floor(Math.random() * words.length)];
-        word = words.splice(Math.floor(Math.random() * words.length), 1)[0];
+        let _index = contents_index.splice(Math.floor(Math.random() * contents_index.length), 1)[0];
+        console.log(contents_index);
+        let _context = sectionContents.ctx.at(_index);
+        console.log(_context);
+        word = _context.word;
         wordUpper = word.toUpperCase();
         wordLower = word.toLowerCase();
         // console.log(words);
         target.textContent = word;
         loc = 0;
+        //
+        // start audio
+        let audioAction = document.querySelector(`audio[data-key="${_context.index}"]`);
+        audioAction.play();
     }
     //
     // ゲーム開始：
@@ -40,7 +55,7 @@
     async function startGame() {
         isPlaying = true;
         // words = targetWords.concat(); // 複製して初期化
-        words = await setWords();
+        sectionContents = await setContents();
         setWord(target);
         result.textContent = "";
         startTime = Date.now();
@@ -74,7 +89,7 @@
             target.textContent = "_".repeat(loc) + word.substring(loc);
         }
         if (loc >=  word.length || (loc == word.length - 1 && word[loc] === "”")) {
-            if (words.length === 0) { // 終了条件
+            if (contents_index.length === 0) { // 終了条件
                 const elapsedTime = ((Date.now() - startTime) / 1000).toFixed(2);
                 result.textContent = `Finished! ${elapsedTime} seconds!`;
                 isPlaying = false;
